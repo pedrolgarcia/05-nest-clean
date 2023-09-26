5492a5e13828751f429974926a2d060c3528e45a
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-09-25 22:51:10 -0300

    feat: authenticate and register student use cases

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -26,29 +26,5 @@ export class CreateAccountController {
   @Post()
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
-  async handle(@Body() body: CreateAccountBodySchema) {
-    const { name, email, password } = body
-
-    const userWithSameEmail = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
-
-    if (userWithSameEmail) {
-      throw new ConflictException(
-        'User with same e-mail address already exists',
-      )
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await this.prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
-    })
-  }
+  async handle(@Body() body: CreateAccountBodySchema) {}
 }
