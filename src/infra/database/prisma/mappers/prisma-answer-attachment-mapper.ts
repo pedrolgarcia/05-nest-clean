731c0336aa407909bbf2ca6e8150b852f35aa7f2
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-09-26 21:26:02 -0300

    feat: prisma mappers

---
+++ src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -0,0 +1,19 @@
+import { Attachment as PrismaAttachment } from '@prisma/client'
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export class PrismaAnswerAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): AnswerAttachment {
+    if (!raw.answerId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return AnswerAttachment.create(
+      {
+        attachmentId: new UniqueEntityId(raw.id),
+        answerId: new UniqueEntityId(raw.answerId),
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+}
---
+++ src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,35 @@
+import { Comment as PrismaComment, Prisma } from '@prisma/client'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaComment): QuestionComment {
+    if (!raw.questionId) {
+      throw new Error('Invalid comment type.')
+    }
+
+    return QuestionComment.create(
+      {
+        content: raw.content,
+        questionId: new UniqueEntityId(raw.questionId),
+        authorId: new UniqueEntityId(raw.authorId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+
+  static toPrisma(
+    questionComment: QuestionComment,
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: questionComment.id.toString(),
+      authorId: questionComment.authorId.toString(),
+      questionId: questionComment.questionId.toString(),
+      content: questionComment.content,
+      createdAt: questionComment.createdAt,
+      updatedAt: questionComment.updatedAt,
+    }
+  }
+}
