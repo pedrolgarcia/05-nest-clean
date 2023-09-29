e776450a49d4c605857418bf06c0d37a7e390494
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-09-28 23:45:13 -0300

    feat: upload attachment base controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -21,6 +21,7 @@ import { CommentOnAnswerController } from './controllers/comment-on-answer.contr
 import { DeleteAnswerCommentController } from './controllers/delete-answer-comment.controller'
 import { FetchQuestionCommentsController } from './controllers/fetch-question-comments.controller'
 import { FetchAnswerCommentsController } from './controllers/fetch-answer-comments.controller'
+import { UploadAttachmentsController } from './controllers/upload-attachment.controller'
 
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
@@ -62,6 +63,7 @@ import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases
     DeleteAnswerCommentController,
     FetchQuestionCommentsController,
     FetchAnswerCommentsController,
+    UploadAttachmentsController,
   ],
   providers: [
     AuthenticateStudentUseCase,
