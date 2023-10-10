fa726469a5c47c7675e4d25da301b33a4d553f7a
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-10-09 23:07:51 -0300

    feat: redis implementation

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common'
+
 import { EnvModule } from '../env/env.module'
-import { EnvService } from '../env/env.service'
+
+import { RedisService } from './redis/redis.service'
+
+import { CacheRepository } from './cache-repository'
+import { RedisCacheRepository } from './redis/redis-cache-repository'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: RedisCacheRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
